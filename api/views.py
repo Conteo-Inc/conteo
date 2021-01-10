d58475b01d93632daabb8ba54066b2c868cc81d9
Jason R. Carrete <jasoncarrete5@gmail.com>
2021-01-10 01:37:37 -0500

    Start work on Matches query set response

--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.models import User
+from django.db.models import Q
 from rest_framework import generics, permissions, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Video
-from .serializers import (UserSerializer, UserSerializerWithToken,
-                          VideoSerializer)
+from .serializers import UserSerializer, UserSerializerWithToken, VideoSerializer
 
 
 @api_view(["GET"])
@@ -36,3 +37,27 @@ class UserList(APIView):
 class VideoListCreate(generics.ListCreateAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
+
+
+class Matches(generics.ListAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        req_user = self.request.user  # The user that is requesting matches
+        return (
+            User.objects.exclude(
+                Q(matchstatus_hi__user_lo=req_user)
+                & (
+                    Q(matchstatus_hi__user_lo_response__isnull=False)
+                    | Q(matchstatus_hi__user_hi_response=False)
+                )
+            )
+            .exclude(
+                Q(matchstatus_lo__user_hi=req_user)
+                & (
+                    Q(matchstatus_lo__user_hi_response__isnull=False)
+                    | Q(matchstatus_lo__user_lo_response=False)
+                )
+            )
+            .exclude(pk=req_user.pk)
+        )
