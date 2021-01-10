ef3e4676b97ad93fe45caeac914b1eb35fd2c887
Jason R. Carrete <jasoncarrete5@gmail.com>
2021-01-09 20:39:42 -0500

    Move common prefix api/ to root urlconf

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,9 @@ from rest_framework_jwt.views import obtain_jwt_token
 from . import views
 
 urlpatterns = [
-    path("api/video/", views.VideoListCreate.as_view()),
-    path("api/token-auth/", obtain_jwt_token),
-    path("api/current_user/", views.current_user),
-    path("api/users/", views.UserList.as_view()),
+    path("video/", views.VideoListCreate.as_view()),
+    path("token-auth/", obtain_jwt_token),
+    path("current_user/", views.current_user),
+    path("users/", views.UserList.as_view()),
+    path("match/", views.Matches.as_view()),
 ]
--- conteo/urls.py
+++ conteo/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("api.urls")),
+    path("api/", include("api.urls")),
     path("", include("frontend.urls")),
 ]
