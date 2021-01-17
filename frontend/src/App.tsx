f304a33527d467240e2ac2f459daae43ed4cbd4d
Jason R. Carrete <jrc394@drexel.edu>
2021-01-16 22:59:49 -0500

    Add Matching page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,35 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import LinkItem from './components/LinkItem';
-import TokenPage from './FullPageRoutes/TokenPage';
+import * as React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LinkItem from "./components/LinkItem";
+import TokenPage from "./FullPageRoutes/TokenPage";
+import MatchingPage from "./FullPageRoutes/Matching";
 
 function MainPage() {
-    return (
-        <ul>
-            <LinkItem to='/Tokens' text='Tokens' />
-        </ul>
-    );
+  return (
+    <ul>
+      <LinkItem to="/Tokens" text="Tokens" />
+    </ul>
+  );
 }
 
 export default function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path='/Tokens'>
-                    <TokenPage />
-                </Route>
-                <Route path='/'>
-                    <MainPage />
-                </Route>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Switch>
+        <Route path="/Tokens">
+          <TokenPage />
+        </Route>
+        <Route path="/matching">
+          <MatchingPage />
+        </Route>
+        <Route path="/">
+          <MainPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
-// export default App;
-
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 render(<App />, container);
