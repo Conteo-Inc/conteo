c43c2be1706f58c399dd50e6f48caedbcae57f11
Michael Needleman <mcn46@drexel.edu>
2021-02-07 19:54:43 -0500

    black & isort

--- conteo/settings.py
+++ conteo/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-from datetime import timedelta
 from pathlib import Path
 
 import environ
