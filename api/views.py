f6f1de8b00241989183f031c8e41c51adc7f8267
Jason R. Carrete <jrc394@drexel.edu>
2021-04-13 01:13:23 -0400

    Simplify matching query

--- api/views.py
+++ api/views.py
@@ -246,29 +246,27 @@ class Matches(viewsets.ModelViewSet):
         as a match.
         """
         req_user = self.request.user
-        # All users that have no matches i.e. not in the MatchStatus table
-        unmatched_users = User.objects.filter(
-            matchstatus_hi__isnull=True, matchstatus_lo__isnull=True
-        ).exclude(pk=req_user.pk)
-        # All users that aren't matched with req_user
         q1 = User.objects.filter(
-            ~Q(matchstatus_hi__user_lo=req_user) & ~Q(matchstatus_lo__user_hi=req_user)
-        ).exclude(pk=req_user.pk)
-        # All users for which user_hi = req_user AND
-        # user_hi has not responded and user_lo has not declined
-        q2 = User.objects.filter(
-            Q(matchstatus_lo__user_hi=req_user)
-            & Q(matchstatus_lo__user_hi_response__isnull=True)
-            & ~Q(matchstatus_lo__user_lo_response=False)
-        ).exclude(pk=req_user.pk)
-        # All users for which user_lo = req_user AND
-        # user_lo has not responded and user_hi has not declined
-        q3 = User.objects.filter(
-            Q(matchstatus_hi__user_lo=req_user)
-            & Q(matchstatus_hi__user_lo_response__isnull=True)
-            & ~Q(matchstatus_hi__user_hi_response=False)
-        ).exclude(pk=req_user.pk)
-        return unmatched_users.union(q1, q2, q3)
+            # Users req_user has responded to
+            Q(
+                matchstatus_hi__user_lo=req_user,
+                matchstatus_hi__user_lo_response__isnull=False,
+            )
+            | Q(
+                matchstatus_lo__user_hi=req_user,
+                matchstatus_lo__user_hi_response__isnull=False,
+            )
+            # Users that have declined req_user
+            | Q(
+                matchstatus_hi__user_lo=req_user,
+                matchstatus_hi__user_hi_response=False
+            )
+            | Q(
+                matchstatus_lo__user_hi=req_user,
+                matchstatus_lo__user_lo_response=False
+            )
+        )
+        return User.objects.exclude(pk=req_user.pk).difference(q1)
 
     def get_object(self):
         match_id = self.request.data["matchId"]
