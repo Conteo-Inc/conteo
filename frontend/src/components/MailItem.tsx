98ec556f94a61ae87175bb29b901f443bb1f8764
Eddie Parrish <elp49@drexel.edu>
2021-05-08 10:48:12 -0400

    using lighter grey

--- frontend/src/components/MailItem.tsx
+++ frontend/src/components/MailItem.tsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles({
     marginBottom: "5px",
   },
   undecidedMatch: {
-    backgroundColor: "grey",
+    backgroundColor: "lightgrey",
     borderRadius: "10px",
   },
 })
