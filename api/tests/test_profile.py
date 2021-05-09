ce11bd9f7c6ab2a48ebc16204af1ac83fe708875
Michael Needleman <mcn46@drexel.edu>
2021-05-09 08:55:43 -0400

    test mail retrieval

--- api/tests/test_profile.py
+++ api/tests/test_profile.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 
-from api.models import Profile
+from api.factory import UserFactory
+from api.models import MatchStatus, Profile
 
 
 class ProfileTest(APITestCase):
@@ -28,3 +29,72 @@ class ProfileTest(APITestCase):
         self.assertEqual(res.status_code, 200)
         u = User.objects.get(username="boy")
         self.assertTrue(u.profile.paused)
+
+
+class MailListTest(APITestCase):
+    def setUp(self):
+        # main user, 3 penpals
+        self.main_user = UserFactory(username="main@main.main", pw="main")
+        self.p1, self.p2, self.p3 = UserFactory.create_batch(pw="test", size=3)
+        # penpal 1 has not matched
+        MatchStatus.objects.create(
+            user_lo=self.main_user,
+            user_hi=self.p1,
+            user_lo_response=True,
+            user_hi_response=None,
+        )
+        # penpal 2 has matched but no video
+        MatchStatus.objects.create(
+            user_lo=self.main_user,
+            user_hi=self.p2,
+            user_lo_response=True,
+            user_hi_response=True,
+        )
+        # penpal 3 has matched and sent video
+        MatchStatus.objects.create(
+            user_lo=self.main_user,
+            user_hi=self.p3,
+            user_lo_response=True,
+            user_hi_response=True,
+        )
+        # It's easier to create the video object through the view than directly
+        # via the model, so for now just text
+        self.video = "video"
+
+    def test_retrieval(self):
+        # Finish setup by sending video from p3 to main
+        # This can be moved back to setup when
+        # we have a good way to construct video objects
+        self.client.login(username=self.p3.username, password="test")
+        self.client.post(
+            "/api/videos/",
+            {"data": self.video, "receiver": self.main_user.id},
+            format="json",
+        )
+
+        # main user logs in and gets mail
+        self.client.login(username="main@main.main", password="main")
+        mail = self.client.get("/api/mail/").json()
+
+        penpals = mail.get("penpals")
+        undecided = mail.get("undecided")
+
+        # expected response structure: {penpals: [2], undecided: [1]}
+        self.assertEqual(len(penpals), 2)
+        self.assertEqual(len(undecided), 1)
+
+        # check order of penpals: p3 first bc of sent video, then p2
+        self.assertEqual(penpals[0].get("id"), self.p3.id)
+        self.assertEqual(penpals[1].get("id"), self.p2.id)
+        self.assertEqual(undecided[0].get("id"), self.p1.id)
+
+        # check viewed_at is None
+        for mailitem in penpals + undecided:
+            self.assertEqual(mailitem.get("viewed_at"), None)
+
+        # check that p1 and p2 created_at is None
+        self.assertEqual(penpals[1].get("created_at"), None)
+        self.assertEqual(undecided[0].get("created_at"), None)
+
+        # check that p3 created_at is not None
+        self.assertFalse(penpals[0].get("created_at") is None)
