b78fe4473672ba29ed93fc333457d08d90cc4be4
Michael Needleman <mcn46@drexel.edu>
2021-05-09 12:11:29 -0400

    video factory

--- api/factory.py
+++ api/factory.py
@@ -1,10 +1,11 @@
 import factory
 from django.contrib.auth.hashers import make_password
+from django.core.files.base import ContentFile
 from django.db.models.signals import post_save
-from factory.helpers import lazy_attribute
+from factory.helpers import lazy_attribute, post_generation
 from factory.random import random
 
-from api.models import MatchStatus, Profile, User
+from api.models import MatchStatus, Profile, User, Video
 
 
 @factory.django.mute_signals()
@@ -65,3 +66,36 @@ class MatchStatusFactory(factory.django.DjangoModelFactory):
 
     class Params:
         response_choices = [True, False, None]
+
+
+class VideoFactory(factory.django.DjangoModelFactory):
+    """Generates a Video object.
+    This factory requires that `sender` and `receiver` be passed in.
+
+    Video src defaults to `"video".encode()`.
+    To customize video src, set `video_file=<some_src>`.
+    """
+
+    created_at = factory.Faker("date")
+    viewed_at = factory.Faker("date")
+
+    @post_generation
+    def video_file(obj, create, extracted):
+        """Generates the video_file after object creation.
+
+        @param obj: the created object.
+
+        @param create: boolean indicating which strategy was used.
+
+        @param extracted: None unless a value was passed in at declaration time.
+        """
+        if not create:
+            return
+
+        data_bytes = extracted.encode() if extracted is not None else "video".encode()
+        fname = "%d_%d_%s" % (obj.sender.id, obj.receiver.id, obj.created_at)
+        cf = ContentFile(data_bytes, name=fname)
+        obj.video_file.save(fname, cf)
+
+    class Meta:
+        model = Video
